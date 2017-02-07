27a2786ae9ea2699fc1dca0965b5d3b6beabd0c4
Bertrand Le Roy <beleroy@microsoft.com>
2017-02-07 15:52:41 -0800

    Move the whole thing to csproj tooling. Refresh versions of everything. Add SkiaSharp to the main NetCore project. Have one solution for both .NET Core projects.

--- NetCore/Program.cs
+++ NetCore/Program.cs
@@ -1,4 +1,10 @@
-﻿using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Toolchains.CsProj;
+using System;
+using System.Linq;
 using System.Reflection;
 
 namespace ImageProcessing
@@ -8,13 +14,41 @@ namespace ImageProcessing
     {
         public static void Main(string[] args)
         {
-            //var lrs = new LoadResizeSave();
-            //lrs.SystemDrawingResizeBenchmark();
-            //lrs.ImageSharpBenchmark();
-            //lrs.MagickResizeBenchmark();
-            //lrs.FreeImageResizeBenchmark();
-            //lrs.MagicScalerResizeBenchmark();
-            new BenchmarkSwitcher(typeof(Program).GetTypeInfo().Assembly).Run(args);
+            var config = ManualConfig.CreateEmpty()
+                   .With(Job.Default.With(CsProjToolchain.NetCoreApp11))
+                   .With(DefaultConfig.Instance.GetLoggers().ToArray())
+                   .With(DefaultConfig.Instance.GetColumnProviders().ToArray())
+                   .With(MemoryDiagnoser.Default);
+            Console.WriteLine(@"Choose an image resizing benchmarks:
+
+0. Just run ""Load, Resize, Save"" once, don't benchmark
+1. Resize
+2. Load, resize, save
+3. Load, resize, save in parallel
+
+");
+            switch(Console.ReadKey().Key)
+            {
+                case ConsoleKey.D0:
+                    var lrs = new LoadResizeSave();
+                    lrs.ImageSharpBenchmark();
+                    lrs.MagickResizeBenchmark();
+                    lrs.FreeImageResizeBenchmark();
+                    lrs.MagicScalerResizeBenchmark();
+                    break;
+                case ConsoleKey.D1:
+                    BenchmarkRunner.Run<Resize>(config);
+                    break;
+                case ConsoleKey.D2:
+                    BenchmarkRunner.Run<LoadResizeSave>(config);
+                    break;
+                case ConsoleKey.D3:
+                    BenchmarkRunner.Run<LoadResizeSaveParallel>(config);
+                    break;
+                default:
+                    Console.WriteLine("Unrecognized command.");
+                    break;
+            }
         }
     }
 }
